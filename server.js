4dcc9db67ea7eed1a3e1694fb6a42ebea9f9d230
Devin Collier <devinlcollier@gmail.com>
2018-04-13 19:43:44 -0700

    change file structure, added code

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const exphbs = require("express-handlebars");
 
 const app = express();
-const PORT = 3000;
+const apiRoutes = require("./app/routing/apiRoutes.js")(app);
+const htmlRoutes = require("./app/routing/htmlRoutes.js")(app);
+
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-const hbs = exphbs.create({
-    defaultLayout: "main",
-    layoutDir: __dirname + "/views/layouts",
-    partialsDir: [
-        __dirname + "/views"
-    ]
-});
-
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
 
 app.listen(PORT, function(){
 	console.log("server started on port " + PORT);
